fb862f082e3d21d9a93f1c1932663b618929901a
David Barcenas <dbarcenas@dbarcen-ltmc97g.internal.salesforce.com>
2022-07-08 12:00:18 -0700

    setup basic auth route

--- lifetracker-api/server.js
+++ lifetracker-api/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const authRouter = require("./routes/auth");
 const app = express();
 const port = 3001;
+
+app.use(cors());
+
+app.use(morgan("tiny"));
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
